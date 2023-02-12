4ee3317063a2cd311bcc7e48ca62e7fc14c75d85
AV_maurits <maurits@avalor.ai>
2023-02-12 20:07:57 +0100

    Some steps with SimpleActor

--- src/actors/any.rs
+++ src/actors/any.rs
@@ -358,10 +358,10 @@ mod tests {
         }
 
         async fn try_test() {
-            let s = S {};
             // let b = f1(s, Box::pin(S::test)).await;
 
-            basic_executer(|s: &S| Box::pin(async move { some_async_call(s).await })).await;
+            basic_executer(|s: &mut S| Box::pin(async move { some_async_call(s).await })).await;
+            dynamic_basic_executer(Box::new(|s: &mut S| Box::pin(async move { some_async_call(s).await }))).await;
         }
     }
 
@@ -371,9 +371,60 @@ mod tests {
 
     async fn basic_executer<F>(callback: F) -> bool
     where
-        F: for<'a> Fn(&'a S) -> BoxFuture<bool>,
+        F: for<'a> Fn(&'a mut S) -> BoxFuture<bool>,
     {
-        let s = S {};
-        callback(&s).await
+        let mut s = S {};
+        (callback)(&mut s).await
+    }
+
+    async fn dynamic_basic_executer(callback: Box<dyn Fn(&mut S) -> BoxFuture<bool>>) -> bool {
+        let mut s = S {};
+        callback(&mut s).await
+    }
+
+    struct SimpleJob<T> {
+        call: Box<dyn Fn(&mut SimpleActor<T>) -> BoxFuture<T>>,
+    }
+
+    struct SimpleHandle<T> {
+        tx: mpsc::Sender<SimpleJob<T>>,
+    }
+
+    impl<T: Clone + Debug + Send + Sync> SimpleHandle<T> {
+        async fn get(&self) {
+            // let res = self.send_job(FnType::Inner(Box::new(Actor::get)), Box::new(())).await?;
+
+            // let job = SimpleJob {
+            //     call: Box::new(|s: SimpleActor<T>| Box::pin(async move { SimpleActor::get(s).await })),
+            // };
+
+            // self.tx.send().await;
+        }
+    }
+
+    struct SimpleActor<T> {
+        inner: T,
+        rx: mpsc::Receiver<SimpleJob<T>>,
+    }
+
+    impl<T: Clone + Debug> SimpleActor<T> {
+        async fn process(&mut self) -> T {
+            let job = self.rx.recv().await.unwrap();
+            let callback = job.call;
+            callback(self).await
+        }
+
+        async fn get(&mut self) -> T {
+            let inner = self.inner.clone();
+            println!("inner {:?}", inner);
+            inner
+        }
+    }
+
+    #[tokio::test]
+    async fn test_simple_actor() {
+        let (tx, rx) = mpsc::channel(CHANNEL_SIZE);
+        let handle = SimpleHandle { tx };
+        let actor = SimpleActor { inner: S {}, rx };
     }
 }
